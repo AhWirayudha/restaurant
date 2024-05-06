da9a17fe8a78f56e35fd0de93d07065f2b666684
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-05-06 13:42:10 +0700

    public function inside module

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-
-        let mut order = serving::take_order();
-        println!("Take order: {}", order);
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 }
 
-// restaurant module front of house
-// mod front_of_house {
-//     mod hosting {
-//         fn add_to_waitlist() {}
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-//         fn seat_at_table() {}
-//     }
-
-//     mod serving {
-//         fn take_order() {}
-
-//         fn serve_order() {}
-
-//         fn take_payment() {}
-//     }
-// }
-
-pub mod front_of_house;
-
-pub use crate::front_of_house::hosting;
-
-pub use crate::front_of_house::serving;
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
