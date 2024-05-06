fec0a72378bdf73ada986c90e49f4c7d6a3bf026
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-05-06 13:04:13 +0700

    example

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,31 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+
+        let mut order = serving::take_order();
+        println!("Take order: {}", order);
     }
 }
 
 // restaurant module front of house
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
-    }
+//         fn seat_at_table() {}
+//     }
 
-    mod serving {
-        fn take_order() {}
+//     mod serving {
+//         fn take_order() {}
 
-        fn serve_order() {}
+//         fn serve_order() {}
 
-        fn take_payment() {}
-    }
-}
+//         fn take_payment() {}
+//     }
+// }
+
+pub mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub use crate::front_of_house::serving;
